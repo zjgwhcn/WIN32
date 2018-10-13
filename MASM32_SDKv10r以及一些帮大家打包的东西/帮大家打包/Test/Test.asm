f8e384272285f65480193e5d1a1cff299ad2047d
zjgwhh <358944052@qq.com>
2018-10-13 09:11:41 +0800

    20181013

---
+++ MASM32_SDKv10r以及一些帮大家打包的东西/帮大家打包/Test/Test.asm
@@ -0,0 +1,41 @@
+;－－
+; Sample code for < Win32ASM Programming 3rd Edition>
+; by 罗云彬, http://www.win32asm.com.cn
+; Change by zjgwhcn, https://zjgwhcn.github.io
+;－－
+; Test.asm
+; 编程环境测试代码
+;－－
+; 使用 nmake 或下列命令进行编译和链接:
+; ml /c /coff Test.asm
+; rc Test.rc
+; Link /subsystem:windows Test.obj Test.res
+;－－
+		.386
+		.model flat,stdcall
+		option casemap:none
+;－－－－－－－－－
+; Include 文件定义
+;－－－－－－－－－
+include		windows.inc
+include		user32.inc
+includelib	user32.lib
+include		kernel32.inc
+includelib	kernel32.lib
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 数据段
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		.const
+
+szCaption	db	'欢迎光临共同学习代码', 0
+szText		db	'来吧，我们一起开始强大的Win32学习之路吧^_^',0
+
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 代码段
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		.code
+start:
+		invoke	MessageBox,NULL,offset szText,offset szCaption,MB_OK
+		invoke	ExitProcess,NULL
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		end	start
